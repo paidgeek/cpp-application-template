3c082bbe5f9a76555f22f5ad65290b47975667e3
paidgeek <tin.rabzelj@gmail.com>
2016-09-12 01:27:18 +0200

    google template

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# cpp-application-template
+# C++ Application Template
+
+Modified from [code.google.com/archive/p/cpp-project-template](https://code.google.com/archive/p/cpp-project-template/).
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,5 @@
 #include <iostream>
 
-int main(int argc, char const *argv[]) {
-	std::cout << "Hello, World!" << std::endl;
-
-	return 0;
+int main(int argc, char *argv[]) {
+  std::cout << "Hello World!" << std::endl;
 }
